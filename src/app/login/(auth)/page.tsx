c40687d300a29a8b266efd4f399c22a4d92d8d7b
yy933 <emilyyu025@gmail.com>
2025-07-22 19:26:01 +0800

    style(Login Page): adjust the login image dark mode background color

--- src/app/login/(auth)/page.tsx
+++ src/app/login/(auth)/page.tsx
@@ -3,7 +3,7 @@ import LoginImage from "@/components/Auth/LoginImage";
 export default function LoginPage() {
   return (
     <main role="main"  className="flex flex-col md:flex-row  min-h-screen p-6 md:p-10">
-      <div className="hidden md:flex h-screen w-full md:w-2/5 max-h-64 md:max-h-full items-center justify-center p-4 bg-gray-100">
+      <div className="hidden md:flex h-screen w-full md:w-2/5 max-h-64 md:max-h-full items-center justify-center p-4 ">
         <LoginImage />
       </div>
       <div className="w-full md:w-3/5 flex items-center justify-center p-4">
